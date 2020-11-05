67b68520ff0cab8b6138d8519c53b5b2c72bb2d5
华伟煌 <weihuang.hua@ucarinc.com>
2020-11-05 17:55:51 +0800

    1

--- src/com/unicss/MySemaphore.java
+++ src/com/unicss/MySemaphore.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
+/**
+ * Semaphore主要用于保护资源
+ */
 public class MySemaphore extends Thread {
 
     Semaphore position;
@@ -23,30 +26,31 @@ public class MySemaphore extends Thread {
                 System.out.println("顾客[" + this.id + "]进入厕所，没空位，排队");
             }
 
-            position.acquire();
-
-            System.out.println("顾客[" + this.id + "]获得坑位");
             Thread.sleep((int) (Math.random() * 1000));
             System.out.println("顾客[" + this.id + "]使用完毕");
 
-            position.release();
+            position.release();//释放资源
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String args[]) {
-        ExecutorService list = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newCachedThreadPool();
         Semaphore position = new Semaphore(2);
 
         for (int i = 0; i < 10; i++) {
-            list.submit(new MySemaphore(i + 1, position));
+            executorService.submit(new MySemaphore(i + 1, position));
         }
 
-        list.shutdown();
+
+        executorService.shutdown();
 
         position.acquireUninterruptibly(2);
+        //有点问题
         System.out.println("使用完毕，需要清扫了");
+        System.out.println("剩余队列大小:" + position.getQueueLength());
         position.release(2);
     }
 }
